525300d1607c93495fb2bd4e2e9a809e7dbe677b
Gabriela Queiroz <gabiqss1@gmail.com>
2023-10-10 22:31:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    let expression = document.getElementById('display').value;
+    let result = eval(expression);
+
+    document.getElementById('display').value = result;
+}
